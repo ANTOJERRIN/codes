d95a8263634911218ca778721c11255ce4873cbd
ANTOJERRIN <jerrinanto0@gmail.com>
2026-08-17 22:54:25 +0530

    files

---
+++ cond.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=45;
+    if(n>=0)
+    {
+        cout<<"n is postitive"<<endl;
+    }
+    if(n==45)
+    {
+        cout<<"equal"<<endl;
+    }
+    else{
+        cout<<"fahhh"<<endl;
+    }
+    return 0;
+}
---
+++ start.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout<<"appna college"<<endl;
+    // cout<<"appna college"<<"\n"<<"from app na collge";
+    cout<<"im jerrin"<<endl<<"anto"<<endl;
+    int a=25;
+    cout<<"my age is "<<a<<endl;
+    cout<<sizeof(a)<<endl;
+    char g='a';
+    cout<<g<<endl;
+    int l=g;
+    cout<<l<<endl;
+    double h=0.99;
+    int j=(int)h;
+    cout<<j<<endl;
+    int user;
+    cin>>user;
+    cout<<"the user limit is "<<user<<endl;
+    cout<<"the sum is "<<(l+user)<<endl<<"the diff"<<(l-user)<<endl;
+    int q=10,f=3;
+    cout<<(q%f)<<endl;
+    cout<<(4>5)<<endl;
+    int r=11;
+    int x=r++;
+    cout<<x<<endl;
+    cout<<r<<endl;
+    return 0;
+}
